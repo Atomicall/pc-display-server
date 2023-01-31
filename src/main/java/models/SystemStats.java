14348154f779f3e670bfdcaa7e3ce409afcd2080
vnu <vnullo@gmail.com>
2023-01-31 18:52:20 +0300

    Init

---
+++ src/main/java/models/SystemStats.java
@@ -0,0 +1,16 @@
+package models;
+
+public class SystemStats {
+    private int cpuTemperature;
+    private int cpuLoad;
+    private int cpuBarPercentage;
+    //in Gb
+    private float ramUsed;
+    private float ramBarPercentage;
+    //in MBps
+    private float networkDownloadRate;
+    private float networkUploadRate;
+    private int networkDownloadBarPercentage;
+    private int networkUploadBarPercentage;
+
+}
