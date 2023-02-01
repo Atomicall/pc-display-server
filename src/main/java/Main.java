3612765b1e7c1a43767f35355cba58021a1103a7
Ященко В.П <d:\The Bat!>
2023-02-01 12:54:51 +0300

    board in progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import oshi.PlatformEnum;
-import oshi.SystemInfo;
 import oshi.hardware.*;
 import oshi.hardware.CentralProcessor.PhysicalProcessor;
 import oshi.hardware.CentralProcessor.ProcessorCache;
@@ -12,6 +11,7 @@ import oshi.software.os.OperatingSystem.ProcessFiltering;
 import oshi.software.os.OperatingSystem.ProcessSorting;
 import oshi.util.FormatUtil;
 import oshi.util.Util;
+import oshi.SystemInfo;
 
 import java.time.Instant;
 import java.util.*;
